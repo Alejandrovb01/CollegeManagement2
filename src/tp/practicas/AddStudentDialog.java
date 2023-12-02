e083312da7b33b26d1ae566f0f11c75f37c9d1c7
alejandro <alejandrovb01@gmail.com>
2023-12-02 18:53:52 +0000

    Initial code implemented.

--- src/tp/practicas/AddStudentDialog.java
+++ src/tp/practicas/AddStudentDialog.java
@@ -1,4 +1,20 @@
 package tp.practicas;
 
-public class AddStudentDialog {
+/**
+ * AVISO: Esta clase puede servir de base para el correspondiente diálogo, pero
+ * no es obligatorio su uso, pudiéndose modificar o eliminar sin restricciones.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+class EnrollStudentDialog extends JDialog {
+    public boolean isAccepted() {
+        return false;
+    }
+    public Student getStudent() {
+        return null;
+    }
+    public Course getCourse() {
+        return null;
+    }
 }
--- src/tp/practicas/CollegeGUI.java
+++ src/tp/practicas/CollegeGUI.java
@@ -1,4 +1,82 @@
 package tp.practicas;
+import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.UIManager.*;
+public class CollegeGUI extends JFrame {
+    static private OfferedCourses offeredCourses= new OfferedCourses();
+    static private EnrolledStudents enrolledStudents = new EnrolledStudents();;
 
-public class CollegeGUI {
+    private JTextArea data;
+    private JRadioButton orderName;
+    private JRadioButton orderId;
+    private JButton addStudent;
+    private JButton enrollStudent;
+
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem itemAddStudent, itemEnrollStudent, itemExit;
+
+    private void constructMenu() {
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        menu = new JMenu("Menu");
+        itemAddStudent = new JMenuItem("Add new student");
+        itemEnrollStudent = new JMenuItem("Enroll student in course");
+        itemExit = new JMenuItem("Exit");
+        menu.add(itemAddStudent);
+        menu.add(itemEnrollStudent);
+        menu.add(itemExit);
+        menuBar.add(menu);
+    }
+    private Component constructControls() {
+        orderName = new JRadioButton("Order by student's name", true);
+        orderId = new JRadioButton("Order by student's id", false);
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(orderName);
+        bg.add(orderId);
+        addStudent = new JButton("Add new student");
+        enrollStudent = new JButton("Enroll student in course");
+        JPanel east = new JPanel();
+        east.setLayout(new BoxLayout(east, BoxLayout.PAGE_AXIS));
+        JPanel order = new JPanel();
+        order.setLayout(new BoxLayout(order, BoxLayout.PAGE_AXIS));
+        order.add(orderName);
+        order.add(orderId);
+        order.setBorder(BorderFactory.createTitledBorder("List order"));
+        east.add(order);
+        east.add(addStudent);
+        east.add(enrollStudent);
+        return east;
+    }
+
+    private Component constructInfo() {
+        data = new JTextArea();
+        data.setEditable(false);
+        return new JScrollPane(data);
+    }
+
+    public CollegeGUI(){
+        super("College Management");
+        InitialData.init(offeredCourses, enrolledStudents);
+        constructMenu();
+        JPanel pane = new JPanel();
+        pane.setLayout(new BorderLayout());
+        pane.add(constructInfo(), BorderLayout.CENTER);
+        pane.add(constructControls(), BorderLayout.EAST);
+        setContentPane(pane);
+        setLocation(50,50);
+        setSize(700,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch(Exception e) {
+        }
+        new CollegeGUI();
+    }
 }
